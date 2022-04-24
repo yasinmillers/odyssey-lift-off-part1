b02de49bd9ff6f22cbdc30cb5c6f65f73817e34c
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-04-24 17:35:52 +0300

    server

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,11 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+    console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at https://studio.apollographql.com/dev
+  `);
+});
