bfa8e6a07abdb5fd60454d3afccace678411e21d
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-04-24 15:59:09 +0300

    track

--- server/src/scheme.js
+++ server/src/scheme.js
@@ -1,3 +1,11 @@
 const { gql } = require('apollo-server');
-const typeDefs = gql ``;
+const typeDefs = gql `
+" track is a collection of modules  thatteaches about specific topic"
+type Track {
+
+
+
+}
+
+`;
 module.exports = typeDefs;
